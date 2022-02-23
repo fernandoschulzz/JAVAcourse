7b4c11d8ae407e73a09aea9ed90dadbb9ccf4504
fernandoschulzz <64163131+fernandoschulzz@users.noreply.github.com>
2022-02-23 12:00:03 -0300

    First Commit

--- src/fernando/methods/Main.java
+++ src/fernando/methods/Main.java
@@ -3,7 +3,7 @@ package fernando.methods;
 public class Main {
 
     public static void main(String[] args) {
-	calculateScore(true,800, 5,100);
+	calculateScore(true,8000, 5,100);
     calculateScore(true, 10000, 8, 200);
     }
     public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus ) {
